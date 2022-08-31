c2240c0f206c32fe8a9df08ac80b0ad2f1ef59e5
Fraianni <francescoianni1995@gmail.com>
2022-08-31 09:46:06 +0200

    porto getelementbyId fuori dal while

--- js/script.js
+++ js/script.js
@@ -3,12 +3,13 @@ const lista_spesa = ['Pane', 'Acqua', 'Pollo'];
 // console.log(lista_spesa);
 
 let i = 0;
+const container = document.getElementById('spesa');
+
 while (i <= lista_spesa.length - 1) {
     const element = document.createElement('p');
     element.append(lista_spesa[i]);
     element.style.color = 'red';
 
-    const container = document.getElementById('spesa');
     container.appendChild(element);
     i++;
 }
